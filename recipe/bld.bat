3a1f52c3076ecf789a3dbd62c6c65d4dfa7f679a
sumanth-manchala <manchala.msr@gmail.com>
2025-08-10 08:45:19 +0530

    Add windows 64bit build

---
+++ recipe/bld.bat
@@ -0,0 +1,23 @@
+@echo off
+
+setlocal
+
+set "outdir=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%"
+if not exist "%outdir%" mkdir "%outdir%"
+if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin"
+
+xcopy * "%outdir%\" /E /I /Q /Y
+
+for %%F in (gcloud gsutil bq docker-credential-gcloud) do (
+    powershell -Command "(Get-Content \"%outdir%\bin\%%F\") -replace '# <cloud-sdk-sh-preamble>', 'set CLOUDSDK_PYTHON=%PREFIX%\bin\python' | Set-Content \"%outdir%\bin\%%F\""
+    mklink "%PREFIX%\bin\%%F" "%outdir%\bin\%%F"
+)
+
+:: google-cloud-sdk starts from %outdir%\lib\googlecloudsdk\core\config.py and
+:: searches for any directory containing .install to mark it as the SDK_ROOT
+:: Empty directories are ignored by the packaging process in Conda, so add a
+:: placeholder to force inclusion of that folder
+if not exist "%outdir%\.install" mkdir "%outdir%\.install"
+type nul > "%outdir%\.install\.conda"
+
+endlocal
